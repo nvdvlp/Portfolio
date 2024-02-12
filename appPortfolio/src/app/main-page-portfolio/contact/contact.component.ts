f30f48100b85b748b348b67163992f1a0783e564
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-02-12 08:38:34 -0600

    early short changes

--- appPortfolio/src/app/main-page-portfolio/contact/contact.component.ts
+++ appPortfolio/src/app/main-page-portfolio/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
