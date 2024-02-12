c3a0d9d4ef3a520106d052d56f5d6f8ea95aa946
nvdvlp <137436428+nvdvlp@users.noreply.github.com>
2024-02-12 11:22:10 -0500

    changes mobile first 12/02/2024

--- appPortfolio/src/app/main-page-portfolio/contact/contact.component.ts
+++ appPortfolio/src/app/main-page-portfolio/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -10,5 +11,6 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  faEnvelope = faEnvelope;
+  faLink = faLink;
 }
--- appPortfolio/src/app/main-page-portfolio/main-view/main-view.component.ts
+++ appPortfolio/src/app/main-page-portfolio/main-view/main-view.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ProyectComponent } from '../proyect/proyect.component';
 import { ContactComponent } from '../contact/contact.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [HeaderComponent, ProyectComponent, ContactComponent],
+  imports: [HeaderComponent, ProyectComponent, ContactComponent, FontAwesomeModule],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.css'
 })
 export class MainViewComponent {
-
+  faArrowRight = faArrowRight;
 }
